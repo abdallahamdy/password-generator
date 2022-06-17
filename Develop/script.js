34f1261c0a9979a2154f9ef250c80085411243f4
AHamdy <abdulla.mh@hotmail.com>
2022-06-16 22:08:42 -0400

    fixed bug when trying to generate multiple passwords

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,87 @@
 // Assignment code here
+var isLowerCase = '';
+var isUpperCase = '';
+var isNumeric = '';
+var isSpecial = '';
 
-var lengthPrompt = function(){
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "1234567890";
+var specialChars = "!@#$%^&*()";
+var passwordCharacters = '';
+
+// Prompt to get desired password length
+function lengthPrompt(){
 
   var passLength = parseInt(window.prompt("Please enter the length of your password (min: 8, max: 128)"));
 
-  return passLength;
+  while(isNaN(passLength) == true || passLength < 8 || passLength > 128){
+  
+    window.alert("The value you've entered is incorrect, please try again.");
+    passLength = lengthPrompt();
 
+    if(isNaN(passLength) == false && passLength > 8 && passLength < 128){
+      break;
+    }
+  }
+  return passLength;
 }
 
-function generatePassword() {
+function characterTypes(){
 
-  var passwordLength = lengthPrompt();
+  isLowerCase = window.confirm("Would you like to include lower case characters?");
+  isUpperCase = window.confirm("Would you like to include uppder case characters?");
+  isNumeric = window.confirm("Would you like to include numeric characters?");
+  isSpecial = window.confirm("Would you like to include special characters?");
 
-  while(isNaN(passwordLength) == true || passwordLength < 8 || passwordLength > 128){
-    
-    window.alert("The value you've entered is incorrect, please try again.");
-    passwordLength = lengthPrompt();
-
-    if(isNaN(passwordLength) == false && passwordLength > 8 && passwordLength < 128){
+  while(isLowerCase == false && isUpperCase == false && isNumeric == false && isSpecial == false){
+    window.alert("Please select at least one character type.");
+    characterTypes();
+    if(isLowerCase == true || isUpperCase == true && isNumeric == true && isSpecial == true){
       break;
     }
   }
 
-  return "the password";
+}
+
+function charGenerator(){
+  passwordCharacters = '';
+  if(isLowerCase){
+    passwordCharacters = passwordCharacters + lowerCaseChars;
+  }
+
+  if(isUpperCase){
+    passwordCharacters = passwordCharacters + upperCaseChars;
+  }
+
+  if(isNumeric){
+    passwordCharacters = passwordCharacters + numericChars;
+  }
+
+  if(isSpecial){
+    passwordCharacters = passwordCharacters + specialChars;
+  }
+}
+
+
+function generatePassword() {
+
+  // get the password length
+  var passwordLength = lengthPrompt();
+
+  // get character types lowercase, uppercase, numeric, and/or special characters
+  characterTypes();
+
+  // when we get required character types, append them into a string
+  charGenerator(); 
+
+  // create the password string from the 
+  var thePassword = '';
+  for(var i = 0; i < passwordLength; i++){
+    thePassword = thePassword + passwordCharacters.charAt(Math.floor(Math.random() * passwordLength));
+  }
+
+  return thePassword;
 }
 
 // Get references to the #generate element
