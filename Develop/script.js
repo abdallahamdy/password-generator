4999830f1b8fe695daba6e554609536fe516489f
AHamdy <abdulla.mh@hotmail.com>
2022-06-16 20:39:44 -0400

    added the password length function and prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+var lengthPrompt = function(){
+
+  var passLength = parseInt(window.prompt("Please enter the length of your password (min: 8, max: 128)"));
+
+  return passLength;
+
+}
+
+function generatePassword() {
+
+  var passwordLength = lengthPrompt();
+
+  while(isNaN(passwordLength) == true || passwordLength < 8 || passwordLength > 128){
+    
+    window.alert("The value you've entered is incorrect, please try again.");
+    passwordLength = lengthPrompt();
+
+    if(isNaN(passwordLength) == false && passwordLength > 8 && passwordLength < 128){
+      break;
+    }
+  }
+
+  return "the password";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
